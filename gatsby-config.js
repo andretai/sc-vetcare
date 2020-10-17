a6bc66c081b87f6dba6a5ca96ad2eaab7a570f99
andretwx <andretai.wx@gmail.com>
2020-10-17 18:09:11 +0800

    Added Heroku config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `VetCare`,
+    description: `Welcome to VetCare's website.`,
+    author: `Andre Tai`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
